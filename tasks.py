7352f8f095864fe671b4e4fecea3e4c448f4c02a
Daniel Kopka <daniel.kopka@syncano.com>
2015-12-17 09:08:26 +0100

    fix for old requests

--- tasks.py
+++ tasks.py
@@ -1,5 +1,6 @@
 import os
 import re
+import json
 import importlib
 import requests
 
@@ -109,6 +110,6 @@ class MarkIssuesTask(celery.Task):
 
     def update_jira_issue(self, issue):
         url = '{}/issue/{}'.format(self.jira_base_url, issue['key'])
-        json = {'fields': {'customfield_10200': issue['customfield_10200']}}
-        response = requests.put(url, json=json, auth=self.jira_auth)
+        data = {'fields': {'customfield_10200': issue['customfield_10200']}}
+        response = requests.put(url, data=json.dumps(data), auth=self.jira_auth)
         response.raise_for_status()
